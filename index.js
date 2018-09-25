db9ac2dedb969def044eb03610e6eebb36449e81
mahirashab18 <mahirashab18@gmail.com>
2018-09-25 11:28:13 +0600

    Coded to a usable level.

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-const reuest = require("request")
-const cheerio = require("cheerio")
-const Spinner = require("cli-spinner").Spinner
-
-const questions = require("./src/inquirer")
+const questions = require("./lib/inquirer")
+const requests = require("./lib/requests")
 
 questions.ask().then(ans => {
     console.log("            ")
 
-    let url = ""
-
-    const obj = new Spinner("processing.. %s")
-    obj.setSpinnerString(18)
-    obj.start()
-
-    if (ans.how === "search by keyword ?") {
-        url = `https://www.imdb.com/search/keyword?keywords=${
-            ans.movieName
-        }&mode=detail&page=1&ref_=kw_ref_key&sort=moviemeter`
+    if (ans.how === "Search by keyword ?") {
+        requests.byKeyword(ans.movieName)
+    } else if (ans.how === "Search by name ?") {
+        requests.byName(ans.movieName)
     }
-
-    console.log(url)
-
-    reuest(url, (err, res, body) => {
-        const $ = cheerio.load(body)
-        obj.stop(true)
-        const data = $(".mode-detail")
-        // .find(".lister-item-header")
-        // .text()
-
-        console.log("            ")
-        console.log("data: " + data)
-    })
 })
